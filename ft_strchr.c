64cfd7918de51e03c466ae153dac329f9e7d1ccf
Ana <anamuelam@gmail.com>
2024-02-02 19:43:58 +0100

    Libft - ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,18 @@
+/*
+DESCRIPCIÓN
+     La función strchr() localiza la primera aparición de c (convertido a un
+     char) en la cadena apuntada por s. El carácter nulo de terminación se
+     considera parte de la cadena; por tanto, si c es '\0', las funciones
+     localizan la terminación '\0'.
+
+VALORES DE RETORNO
+     Las funciones strchr() y strrchr() devuelven un puntero al carácter localizado
+     o NULL si el carácter no aparece en la cadena.
+*/
+
+#include <stdio.h>
+
+char *ft_strchr(const char *s, int i)
+{
+
+}
